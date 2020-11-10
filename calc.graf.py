a45316dfa724838b85aad403116c83baa577a498
sianva <sianva@yandex.ru>
2020-11-10 20:08:20 +0900

    нумпай

--- calc.graf.py
+++ calc.graf.py
@@ -1,4 +1,4 @@
-# **** Генератор паролей **** Tk - объект окна
+# **** Построение сетки по методу 10 мерок **** Tk - объект окна
 from tkinter import Tk, StringVar, Label, Entry, Button
 import hashlib  as h
 
@@ -23,18 +23,28 @@ def hashing():
 # объект окна
 window = Tk()
 # Заголовок окна
-window.title("***Password generator***")
+window.title("***10 Measurement Method***")
 # Переменные с объектами класса StringVar
 pwd = StringVar()
 pwd_hash = StringVar()
 # Текстовая метка, созданная виджетом (компонентом, элементом) класса Label
-label = Label(text="Пароль:")
+label = Label(text="Линия ростка:")
 # Позиционирование виджета методом grid (сетка) где будут располагаться наши виджеты
 label.grid(row=0, column=0, padx=5, pady=5)
 # Поле ввода строки, созданная виджетом Entry
 Entry(textvariable=pwd).grid(row=0, column=1, padx=5, pady=5)
 # Кнопка
 Button(text="Click me", command=hashing).grid(row=1, column=0, padx=5, pady=5)
+
+# Текстовая метка, созданная виджетом (компонентом, элементом) класса Label
+label = Label(text="Пароль:")
+# Позиционирование виджета методом grid (сетка) где будут располагаться наши виджеты
+label.grid(row=1, column=0, padx=5, pady=5)
+# Поле ввода строки, созданная виджетом Entry
+Entry(textvariable=pwd).grid(row=1, column=1, padx=5, pady=5)
+# Кнопка
+Button(text="Click me", command=hashing).grid(row=2, column=0, padx=5, pady=5)
+
 # Поле вывода хеш-строки
-Entry(textvariable=pwd_hash).grid(row=1, column=1, padx=5, pady=5)
+Entry(textvariable=pwd_hash).grid(row=2, column=1, padx=5, pady=5)
 window.mainloop()
